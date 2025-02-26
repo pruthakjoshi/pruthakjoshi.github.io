375e136fb9326b4ccc71246c8b248e9045fb8d98
Pruthak Joshi <35463400+pruthakjoshi@users.noreply.github.com>
2025-02-26 19:24:53 +0530

    Update slideshow.js

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const slides = document.querySelectorAll(".slide");
 
   function showSlides() {
-    slides.forEach((slide) => (slide.style.display = "none"));
+    slides.forEach((slide) => (slide.style.display = "block"));
     slideIndex = (slideIndex + 1) % slides.length;
     slides[slideIndex].style.display = "block";
     setTimeout(showSlides, 3000); // Change image every 3 seconds
